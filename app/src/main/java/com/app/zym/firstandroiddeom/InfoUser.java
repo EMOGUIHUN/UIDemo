6ca8705f8b2ec0b4d5a8ae38f48c17a6e7a9e5d9
EMOGUIHUN <1034963394@qq.com>
2016-12-06 18:52:37 +0800

    no message

--- app/src/main/java/com/app/zym/firstandroiddeom/InfoUser.java
+++ app/src/main/java/com/app/zym/firstandroiddeom/InfoUser.java
@@ -1,10 +1,10 @@
 package com.app.zym.firstandroiddeom;
 
 /**
- * Created by Administrator on 2016/12/5.
+ * Created by Administrator on 2016/12/6.
  */
 
 public class InfoUser {
+    public int haed;
     public String name;
-    public int  haed;
 }
